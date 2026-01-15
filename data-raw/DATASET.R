f8a6a4af7873ee046bb36d8180be8a8784073cb1
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2026-01-15 19:53:10 +0100

    store datasets as dataframe instead of tibble

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,10 +3,12 @@
 continuous_data <- read.csv('./data-raw/CoRRE_continuousTraitData_Apr2024.csv')
 continuous_data <- continuous_data[c('family', 'species', 'trait', 'trait_value')]
 corre_continuous <- tidyr::pivot_wider(continuous_data, names_from=trait, values_from = trait_value)
+corre_continuous <- as.data.frame(corre_continuous)
 
 categorical_data <- read.csv('./data-raw/CoRRE_categoricalTraitData_Nov2023.csv')
 categorical_data <- categorical_data[c('family', 'species', 'trait', 'trait_value')]
 corre_categorical <- tidyr::pivot_wider(categorical_data, names_from=trait, values_from = trait_value)
+corre_categorical <- as.data.frame(corre_categorical)
 
 usethis::use_data(corre_continuous, overwrite = TRUE)
 usethis::use_data(corre_categorical, overwrite = TRUE)
