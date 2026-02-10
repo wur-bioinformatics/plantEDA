7e1dff57d9b455c67a9f66878467c2e814ec57fd
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2026-02-10 13:14:37 +0100

    fix column selection and factor conversion

--- data-raw/corre.R
+++ data-raw/corre.R
@@ -1,13 +1,15 @@
 # Load continuous data csv and convert from long to wide format
 corre_continuous <- read.csv('./data-raw/CoRRE_continuousTraitData_Apr2024.csv') |>
-  dplyr::mutate(dplyr::across(where(is.character), as.factor)) |>
+  dplyr::select(family, species, trait, trait_value) |>
   tidyr::pivot_wider(names_from=trait, values_from = trait_value) |>
+  dplyr::mutate(dplyr::across(where(is.character), as.factor)) |>
   as.data.frame()
 
 # Load categorical data csv and convert from long to wide format
 categorical_data <- read.csv('./data-raw/CoRRE_categoricalTraitData_Nov2023.csv') |>
-  dplyr::mutate(dplyr::across(where(is.character), as.factor)) |>
+  dplyr::select(family, species, trait, trait_value) |>
   tidyr::pivot_wider(names_from=trait, values_from = trait_value) |>
+  dplyr::mutate(dplyr::across(where(is.character), as.factor)) |>
   as.data.frame()
 
 usethis::use_data(corre_continuous, overwrite = TRUE)
