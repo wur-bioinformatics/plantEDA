44364c148648d3cbc7fbfa70f645a3e5c57a3c69
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2026-02-11 20:05:32 +0100

    change species to char

--- data-raw/corre.R
+++ data-raw/corre.R
@@ -2,14 +2,24 @@
 corre_continuous <- read.csv('./data-raw/CoRRE_continuousTraitData_Apr2024.csv') |>
   dplyr::select(family, species, trait, trait_value) |>
   tidyr::pivot_wider(names_from=trait, values_from = trait_value) |>
-  dplyr::mutate(dplyr::across(where(is.character), as.factor)) |>
+  dplyr::mutate(
+    dplyr::across(
+      where(is.character) & !dplyr::any_of('species'),
+      as.factor
+    )
+  ) |>
   as.data.frame()
 
 # Load categorical data csv and convert from long to wide format
 corre_categorical <- read.csv('./data-raw/CoRRE_categoricalTraitData_Nov2023.csv') |>
   dplyr::select(family, species, trait, trait_value) |>
   tidyr::pivot_wider(names_from=trait, values_from = trait_value) |>
-  dplyr::mutate(dplyr::across(where(is.character), as.factor)) |>
+  dplyr::mutate(
+    dplyr::across(
+      where(is.character) & !dplyr::any_of('species'),
+      as.factor
+    )
+  ) |>
   as.data.frame()
 
 usethis::use_data(corre_continuous, overwrite = TRUE)
